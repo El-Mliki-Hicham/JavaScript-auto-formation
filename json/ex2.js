e8da8dbc49824134a9230a0f5a1b35f40c0f085e
hicham <ELmliki.hicham@gmail.com>
2022-01-14 07:58:24 -0800

    add

--- json/ex2.js
+++
@@ -1,7 +0,0 @@
-const xReq = new XMLHttpRequest();
-xReq.onload = function() {
-  const xData = JSON.parse(this.responseText);
-  document.getElementById("para").innerHTML = xData.firstName;
-}
-xReq.open("GET", "ex2.txt");
-xReq.send()
